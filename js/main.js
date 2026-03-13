965752787bd36196e70b0aa3dc004f8d5f7b1875
ookamyabyss <rafaelzinhoccruz@gmail.com>
2026-03-13 05:35:29 -0300

    remove spotify api temporarily

--- js/main.js
+++ js/main.js
@@ -6,3 +6,42 @@ const spotify = document.querySelector(".spotify")
 // simulação visual
 if(xbox) xbox.classList.add("online")
 if(spotify) spotify.classList.add("online")
+
+
+
+function rotateSkills(listClass){
+
+    const list = document.querySelector(`.${listClass}`);
+
+    const skills = Array.from(list.children);
+
+    let index = 0;
+
+    function showNext(){
+
+        skills.forEach(skill => skill.style.display = "none");
+
+        for(let i = index; i < index + 5; i++){
+
+            if(skills[i]){
+                skills[i].style.display = "inline-block";
+            }
+
+        }
+
+        index += 5;
+
+        if(index >= skills.length){
+            index = 0;
+        }
+
+    }
+
+    showNext();
+
+    setInterval(showNext, 60000);
+
+}
+
+rotateSkills("dev-skills");
+rotateSkills("design-skills");
